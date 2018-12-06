4839d03859940295c0d6125b148c15acb0b00d2f
jamie <ilj@infotuity.com>
2018-12-06 23:40:49 +0000

    New quiz

---
+++ quiz3.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Burnside and Vinery Road Allotment Society Quiz - Christmas 2018">
+    <meta name="author" content="">
+    <META HTTP-EQUIV="Pragma" CONTENT="no-cache">
+    <META HTTP-EQUIV="Expires" CONTENT="-1">
+    <link rel="shortcut icon" href="assets/ico/favicon.ico">
+
+    <title>Burnside and Vinery: Quiz, Christmas 2018</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <script src="http://www.w3schools.com/lib/w3data.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/pf_modal.js"></script>
+
+    <!-- Custom styles for this template -->
+    <style>
+        <?php
+            $page = file_get_contents('assets/css/style.css');
+            echo $page;
+        ?>
+    </style>
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    
+    
+  </head>
+
+  <body onload="">
+
+    <!-- Fixed navbar -->
+    <?php include "menu_snippet.html" ?>
+
+	<!-- *****************************************************************************************************************
+	 BLUE WRAP
+	 ***************************************************************************************************************** -->
+	<div id="blue">
+	    <div class="container">
+			<div class="row">
+				<h3>QUIZ - Christmas 2018</h3>
+			</div><!-- /row -->
+	    </div> <!-- /container -->
+	</div><!-- /blue -->
+
+	<!-- *****************************************************************************************************************
+	 CONTACT WRAP
+	 ***************************************************************************************************************** -->
+	 
+	<!-- *****************************************************************************************************************
+	 CONTACT FORMS
+	 ***************************************************************************************************************** -->
+
+	<div class="container">
+
+    <div class="row">
+	 		<div class="col-xs-12">
+        <p class="large-text">Every day we are bombarded with adverts - they are almost literally everywhere.  Well, now they are here too!  Why should we be left out?</p>
+        <p class="large-text">
+          Below are 10 TV adverts from yesteryear.  All you have to do in each case is name the product and the manufacturer. Send your answers to 
+          <a href="mailto:webmaster@burnsideandvineryallotments.org">webmaster@burnsideandvineryallotments.org</a> by the 31st December.  
+          One correct answer will be selected at random for a small (very small) prize.
+        </p>
+      </div>
+    </div>
+
+    <div class="row">
+	 		<div class="col-xs-12 col-sm-6">
+	 			<h4>Question 1</h4>
+         <img class="img img-responsive img-thumbnail" style="width: 100%;" src="assets/img/quiz_xmas_18/1.jpg" />
+      </div>
+      <div class="col-xs-12 col-sm-6">
+	 			<h4>Question 2</h4>
+         <img class="img img-responsive img-thumbnail" style="width: 100%;" src="assets/img/quiz_xmas_18/2.jpg" />
+      </div>
+    </div>
+
+    <div class="row"><div class="col-xs-12">&nbsp;</div></div>
+
+    <div class="row">
+	 		<div class="col-xs-12 col-sm-6">
+	 			<h4>Question 3</h4>
+         <img class="img img-responsive img-thumbnail" style="width: 100%;" src="assets/img/quiz_xmas_18/3.jpg" />
+      </div>
+      <div class="col-xs-12 col-sm-6">
+	 			<h4>Question 4</h4>
+         <img class="img img-responsive img-thumbnail" style="width: 100%;" src="assets/img/quiz_xmas_18/4.jpg" />
+      </div>
+    </div>
+
+    <div class="row"><div class="col-xs-12">&nbsp;</div></div>
+
+    <div class="row">
+	 		<div class="col-xs-12 col-sm-6">
+	 			<h4>Question 5</h4>
+         <img class="img img-responsive img-thumbnail" style="width: 100%;" src="assets/img/quiz_xmas_18/5.jpg" />
+      </div>
+      <div class="col-xs-12 col-sm-6">
+	 			<h4>Question 6</h4>
+         <img class="img img-responsive img-thumbnail" style="width: 100%;" src="assets/img/quiz_xmas_18/6.jpg" />
+      </div>
+    </div>
+
+    <div class="row"><div class="col-xs-12">&nbsp;</div></div>
+
+    <div class="row">
+	 		<div class="col-xs-12 col-sm-6">
+	 			<h4>Question 7</h4>
+         <img class="img img-responsive img-thumbnail" style="width: 100%;" src="assets/img/quiz_xmas_18/7.jpg" />
+      </div>
+      <div class="col-xs-12 col-sm-6">
+	 			<h4>Question 8</h4>
+         <img class="img img-responsive img-thumbnail" style="width: 100%;" src="assets/img/quiz_xmas_18/8.jpg" />
+      </div>
+    </div>
+
+    <div class="row"><div class="col-xs-12">&nbsp;</div></div>
+
+    <div class="row">
+	 		<div class="col-xs-12 col-sm-6">
+	 			<h4>Question 9</h4>
+         <img class="img img-responsive img-thumbnail" style="width: 100%;" src="assets/img/quiz_xmas_18/9.jpg" />
+      </div>
+      <div class="col-xs-12 col-sm-6">
+	 			<h4>Question 10</h4>
+         <img class="img img-responsive img-thumbnail" style="width: 100%;" src="assets/img/quiz_xmas_18/10.jpg" />
+      </div>
+    </div>
+
+    <div class="row"><div class="col-xs-12">&nbsp;</div></div>
+    
+	</div><!--container -->
+	 
+	 
+	<!-- *****************************************************************************************************************
+	 FOOTER
+	 ***************************************************************************************************************** -->
+	 <?php include "footer_snippet.html" ?>
+	 
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <!--script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/retina-1.1.0.js"></script>
+	<script src="assets/js/jquery.hoverdir.js"></script>
+	<script src="assets/js/jquery.hoverex.min.js"></script>
+	<script src="assets/js/jquery.prettyPhoto.js"></script>
+  	<script src="assets/js/jquery.isotope.min.js"></script>
+  	<script src="assets/js/custom.js"></script-->
+
+
+  </body>
+</html>
