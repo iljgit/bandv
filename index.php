86c1b045c47424be9e0abc51efd01d3f57b41653
jamie <ilj@infotuity.com>
2019-09-10 20:36:11 +0100

    Front page pics

--- index.php
+++ index.php
@@ -98,8 +98,8 @@
                     
                     <div class="row">
                         <div class="col-sm-12 col-md-6">
-                            <img src="assets/img/pumpkin2019.jpg" class="img-responsive img-rounded" style="width: 100%;"/>
-                            <p class="text-right"><i>Football's coming home,  Joan - Burnside</i></p>
+                            <img src="assets/img/veg2019_1.jpg" class="img-responsive img-rounded" style="width: 100%;"/>
+                            <p class="text-right"><i>Step 1. Unwrap your pack of toilet rolls...</i></p>
                         </div>
                         <div class="col-sm-12 col-md-6">
                                 <p>Our two allotment sites, Burnside and Vinery Road, are both in the Romsey Town area of south Cambridge. They are run jointly by a single committee. Both sites have full and half plots, although new tenants are now only allotted half plots in the first instance. There are also some quarter plots at Vinery Road.</p>
@@ -128,8 +128,8 @@
                     
                     <div class="row">
                         <div class="col-sm-12 col-md-6">
-                            <img src="assets/img/courgetteflower.jpg" class="img-responsive img-rounded" style="width: 100%;"/>
-                            <p class="text-right"><i>Look at that! Sarah - Burnside</i></p>
+                            <img src="assets/img/veg2019_2.jpg" class="img-responsive img-rounded" style="width: 100%;"/>
+                            <p class="text-right"><i>Step 2. Promise yourself only to eat raw meat in future...</i></p>
                         </div>
                         <div class="col-sm-12 col-md-6">
                             <p>A full plot is approximately 50 metres by 5 metres; a half plot is approximately 25 metres by 5 metres.</p>
