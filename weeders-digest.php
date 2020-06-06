1ab9698b2beddb80b2339700233472ddb790093d
jamie <ilj@infotuity.com>
2020-06-06 11:06:39 +0100

    Typo

--- weeders-digest.php
+++ weeders-digest.php
@@ -168,8 +168,8 @@
                     <div class="card">
                         <div class='card-header' style='background-image: url(/assets/img/weeders-digest/calendar.jpg)'>Calendar</div>
                         <div class="card-body para-highlight">
-                            <p><i><?php echo(getDaysSince('2020-03-23'));?> days since lockdown started.</i></p>
-                            <p><i><?php echo(getDaysToGo('2020-10-25'));?> days until British Summer Time ends.</i></p>
+                            <p><i><?php echo(getDaysSince('2020-03-23'));?> since lockdown started.</i></p>
+                            <p><i><?php echo(getDaysToGo('2020-10-25'));?> until British Summer Time ends.</i></p>
                         </div> 
                         <div class="card-footer"><a href="#calendar" title="What's on the calendar" class='btn btn-success stretched-link'>More...</a></div>
                     </div>
